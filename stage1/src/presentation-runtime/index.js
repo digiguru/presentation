8336314a00867e8a78c817b0c21d370c84cc8550
digiguru <digiguru@gmail.com>
2026-08-13 12:08:52 +0100

    Use first-class GPT input runtime module

--- stage1/src/presentation-runtime/index.js
+++ stage1/src/presentation-runtime/index.js
@@ -1,8 +1,9 @@
 import { installFocusBackground } from './focus-background.js';
-export { installLegacyControls } from './legacy-controls.js';
+import { installGptInput } from './gpt-input.js';
 export { addSlide } from './slides.js';
 
 export function installPresentationRuntime(deck, capabilities = {}) {
   if (capabilities.focusBackground) installFocusBackground(deck);
+  if (capabilities.gptInput) installGptInput(deck);
   return deck;
 }
